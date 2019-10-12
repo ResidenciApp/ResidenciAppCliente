1043c92393c0dcc772be193746e8fbfab409ff6c
lmbaeza <lmbaeza@unal.edu.co>
2019-10-11 21:57:21 -0500

    Se agregaron comentarios sobre los estilos de la barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './views/App/App.css';
 import Navigation from './components/Navigation';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation></Navigation>
+        <Navigation />
       </div>
     );
   }
