07be603602f2d3d16ed45c5ccb36d85fef25e5b9
famedinap <famedinap@unal.edu.co>
2019-11-02 20:12:26 -0500

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
 
+import './Footer.css';
+
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        Footer
+      <div className="page-footer font-small  darken-3">
+        <div class="container">
+
+
+<div class="row">
+
+  
+  <div class="col-md-12 py-5">
+    <div class="mb-5 flex-center">
+
+      
+      <a class="fb-ic">
+        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="tw-ic">
+        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="gplus-ic">
+        <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="li-ic">
+        <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+     
+      <a class="ins-ic">
+        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="pin-ic">
+        <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+      </a>
+    </div>
+  </div>
+  
+</div>
+
+
+</div>
+
+<div class="footer-copyright text-center py-3">
+<a href=""> ...</a>
+</div>
+
       </div>
     )
   }
