45fba00c2cb5bab569130eb9dfdcab0517005e1f
Luis Miguel Baez Aponte <lmbaeza@unal.edu.co>
2019-11-28 14:07:26 -0500

    Se modificó el componente 'NotFound404'

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -17,12 +17,20 @@ class NotFound404 extends Component {
                 <br />
                 <br />
                 <div className="row justify-content-center">
-                    <div className="col-4">
+                    <div className="col-4 text-center">
                         <h1>Not Found
                             <span className="badge badge-secondary">
                                 404
                             </span>
                         </h1>
+
+                        <h3 className="text-center">
+                            <span class="badge badge-primary">
+                                <a className="badge" href="/" style={{color: "#fff"}}>
+                                    <i class="fas fa-home"></i> Ir a la pagina principal
+                                </a>
+                            </span>
+                        </h3>
                     </div>
                 </div>
             </div>
