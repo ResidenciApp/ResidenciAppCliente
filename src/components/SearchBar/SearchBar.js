11e7392384dd1ce40ce4079c9093b0f36ee7af3b
Luis Miguel Baez Aponte <lmbaeza@unal.edu.co>
2019-12-08 12:52:30 -0500

    Se corrigió el css del componente buscador

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,102 +3,91 @@ import {withRouter} from 'react-router-dom';
 import axios from 'axios';
 
 import './SearchBar.css';
-import config from '../../config';
-import $ from 'jquery';
-
-
-
 
 export default class SearchBar extends Component{
    
     render(){
         return(
-            
-            <div class="container py-5 my-5"  >
+        <section class="search-banner text-white py-3 form-search-plan" id="search-banner">
+            <div class="container container-search py-5 my-5"  >
                 <div class="row text-center pb-4">
                      <div class="col-md-12">
-                        <h2 class="text-white siyah-cerceve">¡Busca tu residencia aquí!</h2>
+                        <h2 class="text-white">¡Busca tu residencia <span className="badge badge-residenciapp-green">aquí</span>!</h2>
                     </div>
                  </div>
-            <div class="row">
+                <div class="row">
 
-            <div class="col-md-12">
-                <div class="card acik-renk-form">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h4 class="text-black siyah-cerceve">Ciudad</h4>
-                                <div class="form-group ">
-                                    <select id="iller" class="form-control" >
-                                        <option defaultValue="N">Elegir Ciudad...</option>
-                                        <option >Bogotá</option>
-                                    </select>
+                    <div class="col-md-12">
+                        <div class="card background-form-search">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h4 class="text-black">Ciudad</h4>
+                                        <div class="form-group ">
+                                            <select class="form-control" >
+                                                <option defaultValue="N">Elegir Ciudad...</option>
+                                                <option >Bogotá</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 class="text-black">Localidad</h4>
+                                        <div class="form-group">
+                                            <select id="locality" class="form-control" >
+                                                <option defaultValue="N">Elegir Localidad...</option>
+                                                <option value="15">Antonio Nariño</option>
+                                                <option value="12">Barrios Unidos</option>
+                                                <option value="07">Bosa</option>
+                                                <option value="02">Chapinero</option>
+                                                <option value="19">Ciudad Bolivar</option>
+                                                <option value="10">Engativa</option>
+                                                <option value="09">Fontibón</option>
+                                                <option value="08">Kennedy</option>
+                                                <option value="17">La Candelaria</option>
+                                                <option value="14">Los Mártires</option>
+                                                <option value="16">Puente Aranda</option>
+                                                <option value="18">Rafael Uribe Uribe</option>
+                                                <option value="04">San Cristobal</option>
+                                                <option value="03">Santa Fe</option>
+                                                <option value="11">Suba</option>
+                                                <option value="20">Sumapaz</option>
+                                                <option value="13">Teusaquillo</option>
+                                                <option value="06">Tunjuelito</option>
+                                                <option value="01">Usaquen</option>
+                                                <option value="05">Usme</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                    
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <h4 class="text-black siyah-cerceve">Localidad</h4>
-                                <div class="form-group ">
-                                    <select id="locality" class="form-control" >
-                                        <option defaultValue="N">Elegir Localidad...</option>
-                                        <option value="15">Antonio Nariño</option>
-                                        <option value="12">Barrios Unidos</option>
-                                        <option value="07">Bosa</option>
-                                        <option value="02">Chapinero</option>
-                                        <option value="19">Ciudad Bolivar</option>
-                                        <option value="10">Engativa</option>
-                                        <option value="09">Fontibón</option>
-                                        <option value="08">Kennedy</option>
-                                        <option value="17">La Candelaria</option>
-                                        <option value="14">Los Mártires</option>
-                                        <option value="16">Puente Aranda</option>
-                                        <option value="18">Rafael Uribe Uribe</option>
-                                        <option value="04">San Cristobal</option>
-                                        <option value="03">Santa Fe</option>
-                                        <option value="11">Suba</option>
-                                        <option value="20">Sumapaz</option>
-                                        <option value="13">Teusaquillo</option>
-                                        <option value="06">Tunjuelito</option>
-                                        <option value="01">Usaquen</option>
-                                        <option value="05">Usme</option>
-                                    </select>
-                                </div>
-
-                                
-                            </div>
-                            
-                            
-                            
-                        </div>
-                        <div class="row">
-                            <div class="col-md-8">
-                                    <h4 class="text-black siyah-cerceve">Rango de precio</h4>
-                                    <div class="col-md-4">
-                                        <input
+                                <div class="row">
+                                    <div class="col-md-6">
+                                            <h4 class="text-black siyah-cerceve">Rango de precio</h4>
+                                            <div class="col-md-4">
+                                                <input
                                                     type="number"
                                                     className="form-control"
                                                     aria-label="Default"
                                                     aria-describedby="MinPrice"
                                                     placeholder="Mín."
                                                     name="MinPrice"
-                                        />
-                                    </div>
-                                    <div class="col-md-4">
-                                        <input
+                                                />
+                                            </div>
+                                            <div class="col-md-4">
+                                                <input
                                                     type="number"
                                                     className="form-control"
                                                     aria-label="Default"
                                                     aria-describedby="MaxPrice"
                                                     placeholder="Máx."
                                                     name="MaxPrice"
-                                        />
+                                                />
+                                            </div>
                                     </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <h4 class="text-black siyah-cerceve">Servicios Incluidos</h4>
-                        </div>
-                        <div class="row">
+                                <div className="col-md-6">
+                                <h4 class="text-black siyah-cerceve">Servicios Incluidos</h4>
                                 <div className="input-group-prepend" >
                                 <span>
                                     <input type="checkbox" name="includedServices" value="1" onChange={this.handleChange} /> Alimentación<br></br>
@@ -109,14 +98,12 @@ export default class SearchBar extends Component{
                                     <input type="checkbox" name="includedServices" value="6" onChange={this.handleChange} /> Cocina disponible<br></br>
                                     <input type="checkbox" name="includedServices" value="7" onChange={this.handleChange} /> Baño privado<br></br>
                                 </span>
-                                
-
-                                
+                                </div>
                             </div>
                         </div>
                         
                         <div class="row">
-                            <h4 class="text-black siyah-cerceve">Nombre</h4>
+                            <h4 class="text-black">Nombre</h4>
                         </div>
                         <div class="row">
                             <div class="col-md-9">
@@ -134,16 +121,8 @@ export default class SearchBar extends Component{
                 </div>
             </div>
         </div>
-    
-
-        
+        </section>
         )
-        $('#langOpt').multiselect({
-            columns: 1,
-            placeholder: 'Servicios',
-            search: true,
-            selectAll: true
-        }); 
     }    
     
 }
