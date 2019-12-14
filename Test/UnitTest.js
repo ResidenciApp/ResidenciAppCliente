879f26164a1fd353f06262edbd9d56a2edc3b6d4
famedinap <famedinap@unal.edu.co>
2019-12-14 12:23:51 -0500

    corre el test de OwnerProfile

--- Test/UnitTest.js
+++ Test/UnitTest.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import footer from 'src/components/Footer/Footer';
 import {shallow } from 'enzyme';
-
-import OwnerProfile from './../OwnerProfile';
 
 import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({adapter: new Adapter()});
 
-
-
-it("Renders without crashing", () =>{
-
-    shallow(<OwnerProfile />);
-})
+it('Renders without crashing', () => {
+    shallow(<footer />);
+});
